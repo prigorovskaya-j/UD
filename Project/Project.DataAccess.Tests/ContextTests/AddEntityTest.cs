d00c5b599574534bddf555e4b0b2cedfa8a02018
Petro <kolosovp94@gmail.com>
2021-01-07 05:50:21 +0100

    context tested

--- Project/Project.DataAccess.Tests/ContextTests/AddEntityTest.cs
+++ Project/Project.DataAccess.Tests/ContextTests/AddEntityTest.cs
@@ -1,10 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using Project.DataAccess.Context;
+using Project.DataAccess.Models;
 
 namespace Project.DataAccess.Tests.ContextTests
 {
     [TestFixture]
     public class AddEntityTest
     {
-        
+        [Test]
+        public void Add_Document_Test()
+        {
+            var context = new UDataBaseContext();
+            var toAdd = new Document
+            {
+                InventoryName = "Stol",
+                DurationOfUse = 2,
+                DateUsedFrom = DateTime.Now,
+                Reason = "I want it"
+            };
+            context.Documents.Add(toAdd);
+            context.SaveChanges();
+        }
     }
 }
