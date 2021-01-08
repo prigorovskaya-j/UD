08c1d73db229a760c8cadbb8a57b7759e7410eb5
Petro <kolosovp94@gmail.com>
2021-01-08 02:53:25 +0100

    project updated

--- Project/Project.DataAccess/Connection/ConnectionStrings.cs
+++ Project/Project.DataAccess/Connection/ConnectionStrings.cs
@@ -4,7 +4,6 @@
     {
         public const string MyConnectionString = "Data Source=DESKTOP-P87PH2B;Initial Catalog=ProjectDataBase;Integrated Security=true;";
         public const string NewMyConnectionString = "Data Source=DESKTOP-6UT853M;Initial Catalog=ProjectDataBase;Integrated Security=true;";
-
        
     }
 }
